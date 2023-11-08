aad8833ada1696ed5b83be73429e15506fb0bd87
Rofus06 <Filipsmail@yahoo.com>
2023-11-08 11:07:40 +0100

    beginning

--- TimeToDrive/Program.cs
+++ TimeToDrive/Program.cs
@@ -1,1 +1,23 @@
 ﻿using System;
+
+int hp;
+int x = 0;
+int y = 0;
+int score = 0;
+Console.SetCursorPosition (x, y);
+
+Console.WriteLine("Hello and Welcome To My Game TimeToDeive");
+Console.ReadLine();
+Console.WriteLine("The only thing you need to do is to stay on the road");
+Console.ReadLine();
+Console.WriteLine("You are going to use these keys: W, A, S, D");
+Console.ReadLine();
+
+Console.Write("press enter to start");
+while (true)
+{
+    
+}
+
+
+
